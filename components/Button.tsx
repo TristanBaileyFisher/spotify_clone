2580af832a8453f96e458ced15a1ceef465e04bd
Tristan Bailey Fisher <tristanfisher@outlook.co.nz>
2024-12-12 10:16:16 +1300

    more

--- components/Button.tsx
+++ components/Button.tsx
@@ -9,7 +9,8 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
       <button
         type={type}
         className={twMerge(
-          "w-full rounded-full bg-green-500 border border-transparent px-3 py-3 disabled:cursor-not-allowed siabled:opacity-50 text-black font-bol hover:opacity-75 transition"
+          "w-full rounded-full bg-green-500 border border-transparent px-3 py-3 disabled:cursor-not-allowed disabled:opacity-50 text-black font-bold hover:opacity-75 transition",
+          className // Merge external classes
         )}
         disabled={disabled}
         ref={ref}
